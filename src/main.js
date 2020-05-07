f40c7a3c5b5044b07384dec72150025b9d23d143
Eanew <H.Eanew@yandex.ru>
2020-05-08 02:36:54 +0300

    Исправлена ошибка и проблема со stylelint

--- src/main.js
+++ src/main.js
@@ -133,7 +133,7 @@ const createDetailsTemplate = () => {
     `<section class="film-details">
       <form class="film-details__inner" action="" method="get"></form>
     </section>`
-  )
+  );
 };
 
 const createDetailsTopContainerTemplate = () => {
